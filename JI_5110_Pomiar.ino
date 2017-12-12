fc4f17aedd129327870e535b0a39c33ed6643034
Your Name <you@example.com>
2017-12-12 17:55:08 +0100

    first commit

---
+++ JI_5110_Pomiar.ino
@@ -0,0 +1,582 @@
+/*********************************************************************
+This is an example sketch for our Monochrome Nokia 5110 LCD Displays
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/338
+
+These displays use SPI to communicate, 4 or 5 pins are required to
+interface
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+#include <Wire.h>
+#include <SPI.h>
+#include <BH1750.h>
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+
+// Hardware SPI (faster, but must use certain hardware pins):
+// SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
+// MOSI is LCD DIN - this is pin 11 on an Arduino Uno
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+// Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+// Note with hardware SPI MISO and SS pins aren't used but will still be read
+// and written to during SPI transfer.  Be careful sharing these pins!
+BH1750 lightMeter;
+
+#define BME280_ADDRESS 0x76
+unsigned long int hum_raw,temp_raw,pres_raw;
+signed long int t_fine;
+
+uint16_t dig_T1;
+ int16_t dig_T2;
+ int16_t dig_T3;
+uint16_t dig_P1;
+ int16_t dig_P2;
+ int16_t dig_P3;
+ int16_t dig_P4;
+ int16_t dig_P5;
+ int16_t dig_P6;
+ int16_t dig_P7;
+ int16_t dig_P8;
+ int16_t dig_P9;
+ int8_t  dig_H1;
+ int16_t dig_H2;
+ int8_t  dig_H3;
+ int16_t dig_H4;
+ int16_t dig_H5;
+ int8_t  dig_H6;
+
+
+
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+int a= 32534;
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void setup()   {
+  Serial.begin(9600);
+
+     uint8_t osrs_t = 1;             //Temperature oversampling x 1
+    uint8_t osrs_p = 1;             //Pressure oversampling x 1
+    uint8_t osrs_h = 1;             //Humidity oversampling x 1
+    uint8_t mode = 3;               //Normal mode
+    uint8_t t_sb = 5;               //Tstandby 1000ms
+    uint8_t filter = 0;             //Filter off 
+    uint8_t spi3w_en = 0;           //3-wire SPI Disable
+    
+    uint8_t ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode;
+    uint8_t config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en;
+    uint8_t ctrl_hum_reg  = osrs_h;
+
+
+  
+  lightMeter.begin();  //inicjacja pomiaru oswietlenia
+  Serial.println("Running..."); // Drukowanie na Terminalu
+
+  display.begin();
+  // init done
+
+  // you can change the contrast around to adapt the display
+  // for the best viewing!
+  display.setContrast(55);    //bylo na poczatku 50
+
+ // display.display(); // show splashscreen
+ // delay(1000);
+  display.clearDisplay();   // clears the screen and buffer
+
+    writeReg(0xF2,ctrl_hum_reg);
+    writeReg(0xF4,ctrl_meas_reg);
+    writeReg(0xF5,config_reg);
+    readTrim(); 
+
+  // draw a single pixel
+//  display.drawPixel(10, 10, BLACK);
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw many lines
+//  testdrawline();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw rectangles
+//  testdrawrect();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw multiple rectangles
+//  testfillrect();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw mulitple circles
+//  testdrawcircle();
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw a circle, 10 pixel radius
+//  display.fillCircle(display.width()/2, display.height()/2, 10, BLACK);
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testdrawroundrect();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testfillroundrect();
+//  delay(2000);
+//  display.clearDisplay();
+//
+//  testdrawtriangle();
+//  delay(2000);
+//  display.clearDisplay();
+   
+//  testfilltriangle();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // draw the first ~12 characters in the font
+//  testdrawchar();
+//  display.display();
+//  delay(10000);
+//  display.clearDisplay();
+
+  // text display tests
+//  display.setTextSize(1);
+//  display.setTextColor(BLACK);
+//  display.setCursor(0,0);
+//  display.println("Hello, Jurek!");
+ // display.setTextColor(WHITE, BLACK); // 'inverted' text
+//  display.println(3,141592);
+//  display.setTextSize(2);
+//  display.setTextColor(BLACK);
+//  display.print("0x"); display.println(0xDEADB, HEX);
+//  display.println("Zosia");
+//  display.display();
+//  delay(2000);
+//  display.clearDisplay();
+
+  // rotation example
+//  display.clearDisplay();
+//  display.setRotation(0);  // rotate 90 degrees counter clockwise, can also use values of 2 and 3 to go further.
+//  display.setTextSize(1);
+//  display.setTextColor(BLACK);
+//  display.setCursor(0,0);
+//  display.println("Rotation");
+//  display.setTextSize(2);
+//  display.println("Example!");
+//  display.display();
+//  delay(2000);
+
+  // revert back to no rotation
+ // display.setRotation(0);
+
+  // miniature bitmap display
+//  display.clearDisplay();
+//  display.drawBitmap(30, 16,  logo16_glcd_bmp, 16, 16, 1);
+//  display.display();
+
+  // invert the display
+//  display.invertDisplay(true);
+//  delay(1000); 
+//  display.invertDisplay(false);
+//  delay(1000); 
+
+  // draw a bitmap icon and 'animate' movement
+//  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_WIDTH, LOGO16_GLCD_HEIGHT);
+
+}
+
+ void loop() {
+
+      double temp_act = 0.0, press_act = 0.0,hum_act=0.0;
+    signed long int temp_cal;
+    unsigned long int press_cal,hum_cal;
+    
+    readData();
+    
+    temp_cal = calibration_T(temp_raw);
+    press_cal = calibration_P(pres_raw);
+    hum_cal = calibration_H(hum_raw);
+    temp_act = (double)temp_cal / 100.0;
+    press_act = (double)press_cal / 100.0;
+    hum_act = (double)hum_cal / 1024.0;
+    Serial.print("TEMP : ");
+    Serial.print(temp_act);
+    Serial.print(" DegC  PRESS : ");
+    Serial.print(press_act);
+    Serial.print(" hPa  HUM : ");
+    Serial.print(hum_act);
+    Serial.println(" %");    
+
+  
+   uint16_t lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+
+  
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0); //5 przesuniecie w prawo, 2 przesuniecie do dolu
+  display.println("Pomiary");
+  display.println(" ");
+  display.print("Tem:");
+  display.print(temp_act);
+  display.println(" oC");
+  display.print("Cis:");
+  display.print(press_act);
+  display.println("hPa");
+  display.print ("Wilg:  ");
+  display.print(hum_act);
+  display.println(" %");    
+  display. print("Jasn: ");
+  display.print(lux);
+  display.println(" lx");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+   }
+
+//*************Funkcje****************
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  uint8_t icons[NUMFLAKES][3];
+  randomSeed(666);     // whatever seed
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random(display.width());
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random(5) + 1;
+    
+    Serial.print("x: ");
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(" y: ");
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(" dy: ");
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  while (1) {
+    // draw each icon
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, BLACK);
+    }
+    display.display();
+    delay(200);
+    
+    // then erase it + move it
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, WHITE);
+      // move it
+      icons[f][YPOS] += icons[f][DELTAY];
+      // if its gone, reinit
+      if (icons[f][YPOS] > display.height()) {
+	icons[f][XPOS] = random(display.width());
+	icons[f][YPOS] = 0;
+	icons[f][DELTAY] = random(5) + 1;
+      }
+    }
+   }
+}
+
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    //if ((i > 0) && (i % 14 == 0))
+      //display.println();
+  }    
+  display.display();
+}
+
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
+    display.display();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (int16_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.display();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (int16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, BLACK);
+    display.display();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = BLACK;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+
+void testdrawroundrect(void) {
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, BLACK);
+    display.display();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = BLACK;
+  for (int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.display();
+  }
+}
+   
+void testdrawrect(void) {
+  for (int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, BLACK);
+    display.display();
+  }
+}
+
+void testdrawline() {  
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, BLACK);
+    display.display();
+  }
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int8_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
+    display.display();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int16_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
+    display.display();
+  }
+  for (int16_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
+    display.display();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (int16_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, BLACK);
+    display.display();
+  }
+  for (int16_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK); 
+    display.display();
+  }
+  delay(250);
+}
+
+void readTrim()
+{
+    uint8_t data[32],i=0;                      // Fix 2014/04/06
+    Wire.beginTransmission(BME280_ADDRESS);
+    Wire.write(0x88);
+    Wire.endTransmission();
+    Wire.requestFrom(BME280_ADDRESS,24);       // Fix 2014/04/06
+    while(Wire.available()){
+        data[i] = Wire.read();
+        i++;
+    }
+    
+    Wire.beginTransmission(BME280_ADDRESS);    // Add 2014/04/06
+    Wire.write(0xA1);                          // Add 2014/04/06
+    Wire.endTransmission();                    // Add 2014/04/06
+    Wire.requestFrom(BME280_ADDRESS,1);        // Add 2014/04/06
+    data[i] = Wire.read();                     // Add 2014/04/06
+    i++;                                       // Add 2014/04/06
+    
+    Wire.beginTransmission(BME280_ADDRESS);
+    Wire.write(0xE1);
+    Wire.endTransmission();
+    Wire.requestFrom(BME280_ADDRESS,7);        // Fix 2014/04/06
+    while(Wire.available()){
+        data[i] = Wire.read();
+        i++;    
+    }
+    dig_T1 = (data[1] << 8) | data[0];
+    dig_T2 = (data[3] << 8) | data[2];
+    dig_T3 = (data[5] << 8) | data[4];
+    dig_P1 = (data[7] << 8) | data[6];
+    dig_P2 = (data[9] << 8) | data[8];
+    dig_P3 = (data[11]<< 8) | data[10];
+    dig_P4 = (data[13]<< 8) | data[12];
+    dig_P5 = (data[15]<< 8) | data[14];
+    dig_P6 = (data[17]<< 8) | data[16];
+    dig_P7 = (data[19]<< 8) | data[18];
+    dig_P8 = (data[21]<< 8) | data[20];
+    dig_P9 = (data[23]<< 8) | data[22];
+    dig_H1 = data[24];
+    dig_H2 = (data[26]<< 8) | data[25];
+    dig_H3 = data[27];
+    dig_H4 = (data[28]<< 4) | (0x0F & data[29]);
+    dig_H5 = (data[30] << 4) | ((data[29] >> 4) & 0x0F); // Fix 2014/04/06
+    dig_H6 = data[31];                                   // Fix 2014/04/06
+}
+void writeReg(uint8_t reg_address, uint8_t data)
+{
+    Wire.beginTransmission(BME280_ADDRESS);
+    Wire.write(reg_address);
+    Wire.write(data);
+    Wire.endTransmission();    
+}
+
+
+void readData()
+{
+    int i = 0;
+    uint32_t data[8];
+    Wire.beginTransmission(BME280_ADDRESS);
+    Wire.write(0xF7);
+    Wire.endTransmission();
+    Wire.requestFrom(BME280_ADDRESS,8);
+    while(Wire.available()){
+        data[i] = Wire.read();
+        i++;
+    }
+    pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
+    temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+    hum_raw  = (data[6] << 8) | data[7];
+}
+
+
+signed long int calibration_T(signed long int adc_T)
+{
+    
+    signed long int var1, var2, T;
+    var1 = ((((adc_T >> 3) - ((signed long int)dig_T1<<1))) * ((signed long int)dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - ((signed long int)dig_T1)) * ((adc_T>>4) - ((signed long int)dig_T1))) >> 12) * ((signed long int)dig_T3)) >> 14;
+    
+    t_fine = var1 + var2;
+    T = (t_fine * 5 + 128) >> 8;
+    return T; 
+}
+
+unsigned long int calibration_P(signed long int adc_P)
+{
+    signed long int var1, var2;
+    unsigned long int P;
+    var1 = (((signed long int)t_fine)>>1) - (signed long int)64000;
+    var2 = (((var1>>2) * (var1>>2)) >> 11) * ((signed long int)dig_P6);
+    var2 = var2 + ((var1*((signed long int)dig_P5))<<1);
+    var2 = (var2>>2)+(((signed long int)dig_P4)<<16);
+    var1 = (((dig_P3 * (((var1>>2)*(var1>>2)) >> 13)) >>3) + ((((signed long int)dig_P2) * var1)>>1))>>18;
+    var1 = ((((32768+var1))*((signed long int)dig_P1))>>15);
+    if (var1 == 0)
+    {
+        return 0;
+    }    
+    P = (((unsigned long int)(((signed long int)1048576)-adc_P)-(var2>>12)))*3125;
+    if(P<0x80000000)
+    {
+       P = (P << 1) / ((unsigned long int) var1);   
+    }
+    else
+    {
+        P = (P / (unsigned long int)var1) * 2;    
+    }
+    var1 = (((signed long int)dig_P9) * ((signed long int)(((P>>3) * (P>>3))>>13)))>>12;
+    var2 = (((signed long int)(P>>2)) * ((signed long int)dig_P8))>>13;
+    P = (unsigned long int)((signed long int)P + ((var1 + var2 + dig_P7) >> 4));
+    return P;
+}
+
+unsigned long int calibration_H(signed long int adc_H)
+{
+    signed long int v_x1;
+    
+    v_x1 = (t_fine - ((signed long int)76800));
+    v_x1 = (((((adc_H << 14) -(((signed long int)dig_H4) << 20) - (((signed long int)dig_H5) * v_x1)) + 
+              ((signed long int)16384)) >> 15) * (((((((v_x1 * ((signed long int)dig_H6)) >> 10) * 
+              (((v_x1 * ((signed long int)dig_H3)) >> 11) + ((signed long int) 32768))) >> 10) + (( signed long int)2097152)) * 
+              ((signed long int) dig_H2) + 8192) >> 14));
+   v_x1 = (v_x1 - (((((v_x1 >> 15) * (v_x1 >> 15)) >> 7) * ((signed long int)dig_H1)) >> 4));
+   v_x1 = (v_x1 < 0 ? 0 : v_x1);
+   v_x1 = (v_x1 > 419430400 ? 419430400 : v_x1);
+   return (unsigned long int)(v_x1 >> 12);   
+}
+
+
